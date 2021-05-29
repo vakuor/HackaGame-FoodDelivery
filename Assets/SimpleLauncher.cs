b255b052ca760675734393d7121e1816f690bd6a
Vladislav vakuor <vakuor@yandex.ru>
2021-05-29 05:24:33 +0300

    ServerBuild

---
+++ Assets/SimpleLauncher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SimpleLauncher : MonoBehaviour
+{
+    NetworkManager manager;
+    private void Start() {
+        manager = GetComponent<NetworkManager>();
+        manager.StartServer();
+    }
+}
