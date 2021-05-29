f7c5642a6af32aa5d5e6e58399c4c1b126796059
Vladislav vakuor <vakuor@yandex.ru>
2021-05-29 05:00:55 +0300

    Initial commit

---
+++ Assets/SimpleMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SimpleMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    float step = 1f;
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)) { transform.SetPositionAndRotation(new Vector3(transform.position.x,transform.position.y + step * Time.deltaTime, transform.position.z), Quaternion.identity); 
+
+        }
+    }
+}
